edb28c1ca6ecf0108208d1d94c0b9825f798e5be
DanilV <dani736@gmail.com>
2022-07-24 23:36:21 +0300

    finished weather command

--- cities.py
+++ cities.py
@@ -1,9 +1,13 @@
-import geonamescache
+import mysql.connector
 
-gc = geonamescache.GeonamesCache()
-countries = gc.get_cities_by_name()
-# print countries dictionary
-a= 'israel'
-if a.capitalize() in countries.keys():
-    print(True)
-# print(countries.keys())
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user="root",
+    passwd = "root"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SHOW DATABASES")
+for dc in mycursor:
+    print(dc)
--- test_proj_bot1.py
+++ test_proj_bot1.py
@@ -2,11 +2,8 @@ import telebot
 import http.client
 import urllib.parse
 import requests
-from telebot import types
 from requests import get
-import geonamescache
 import json
-
 
 API_KEY_TELE = "5155568476:AAHC3_vVjKjVwzL-zLXejc6SfVVWV1u4_FE"
 
@@ -17,10 +14,43 @@ API_KEY_LOCATION = "05fe4fc8294f68ce34f808282d69b31f"
 url_location = "https://api.ambeedata.com/weather/latest/by-lat-lng"
 
 bot = telebot.TeleBot(API_KEY_TELE)
-# gc = geonamescache.GeonamesCache()
-# countries = gc.get_cities_by_name()
 
-mydict = {}
+@bot.message_handler(commands=['start'])
+def help_start(message):
+    bot.send_message(message.chat.id, 'Welcome to Danils bot!\nEnter /help for commands')
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id, "Avilable commands:\n/weather")
+
+@bot.message_handler(commands=['weather'])
+def show_weather(message):
+    msg = bot.send_message(message.chat.id, "Enter city to see weather conditions:")
+    bot.register_next_step_handler(msg, location_name)
+
+def location_name(message):
+    global lat,lng
+    conn = http.client.HTTPConnection('api.positionstack.com')
+    params = urllib.parse.urlencode({
+        'access_key': f'{API_KEY_LOCATION}',
+        'query': f'{message.text}',
+        'limit': 1,
+        'output': 'json'
+    })
+    conn.request('GET', f'/v1/forward?{params}')   
+    res = conn.getresponse().read()
+    dec = res.decode()
+    ajson = json.loads(dec)
+    print((len(ajson["data"])))
+    if len(ajson["data"]) > 0:
+        lat = ajson['data'][0]['latitude']
+        lng = ajson['data'][0]['longitude']
+        country_name = ajson['data'][0]['country']
+        print(lat,',',lng)
+        bot.send_message(message.chat.id, f'Weather in {country_name}, {message.text.capitalize()} {weather()} celcius')
+        return lat, lng
+    else:
+        bot.send_message(message.chat.id, "No such city found!\nTry again /weather")
 
 def weather():
     querystring = {"lat": f"{lat}", "lng": f"{lng}"}
@@ -30,59 +60,7 @@ def weather():
     }
     response = requests.request(
         "GET", url_location, headers=headers, params=querystring).json()
-    res =  round(((response['data']['apparentTemperature'])-32)*5/9)
+    res = round(((response['data']['apparentTemperature'])-32)*5/9)
     return res
-
-@bot.message_handler(commands=['weather'])
-def show_weather(message):
-    msg = bot.send_message(message.chat.id, "enter location")
-    bot.register_next_step_handler(msg, location_name)
-
-
-def location_name(message):
-    # if message.text.lower() == "yes":
-        global lat,lng
-        conn = http.client.HTTPConnection('api.positionstack.com')
-        params = urllib.parse.urlencode({
-            'access_key': f'{API_KEY_LOCATION}',
-            'query': f'{message.text}',
-            'limit': 1,
-            'output': 'json'
-        })
-        conn.request('GET', f'/v1/forward?{params}')
-
-        res = conn.getresponse().read()
-        dec = res.decode()
-        ajson = json.loads(dec)
-        lat = ajson['data'][0]['latitude']
-        lng = ajson['data'][0]['longitude']
-        print(lat,lng)
-        ls = [lat,lng]
-        # mydict[lat] = lng
-        bot.send_message(message.chat.id, weather())
-        return lat, lng
-    # elif message.text.lower() == "no":
-    #     bot.send_message(message.chat.id, "good bye")
-
-# def get_location(message):
-#     global lat, lng
-#     msg = bot.register_next_step_handler(message, weather)
-#     conn = http.client.HTTPConnection('api.positionstack.com')
-#     params = urllib.parse.urlencode({
-#         'access_key': f'{API_KEY_LOCATION}',
-#         'query': f'{msg}',
-#         'limit': 1,
-#         'output':'json'
-#         })
-
-#     conn.request('GET', f'/v1/forward?{params}')
-
-#     res = conn.getresponse().read()
-#     dec = res.decode()
-#     ajson = json.load(dec)
-#     lat = ajson['data'][0]['latitude']
-#     lng = ajson['data'][0]['longitude']
-#     return lat, lng
-
 
 bot.polling()
